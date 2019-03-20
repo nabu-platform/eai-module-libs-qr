8c2eb9f89b6447b9837102bc5262bafd2f6ce3f2
Alexander Verbruggen <alex@nabu.be>
2019-03-20 16:21:36 +0100

    initial

---
+++ src/main/java/nabu/libs/misc/qr/Services.java
@@ -0,0 +1,46 @@
+package nabu.libs.misc.qr;
+
+import io.nayuki.qrcodegen.QrCode;
+
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+import java.util.Iterator;
+
+import javax.imageio.ImageIO;
+import javax.imageio.ImageWriter;
+import javax.imageio.stream.ImageOutputStream;
+import javax.jws.WebParam;
+import javax.jws.WebResult;
+import javax.jws.WebService;
+
+@WebService
+public class Services {
+	@WebResult(name = "svg")
+	public InputStream toSvg(@WebParam(name = "string") String text) {
+		QrCode qr = QrCode.encodeText("Hello, world!", QrCode.Ecc.MEDIUM);
+		return new ByteArrayInputStream(qr.toSvgString(4).getBytes(Charset.forName("UTF-8")));
+	}
+	@WebResult(name = "image")
+	public InputStream toImage(@WebParam(name = "string") String text, @WebParam(name = "contentType") String contentType) throws IOException {
+		if (contentType == null) {
+			contentType = "image/png";
+		}
+		QrCode qr = QrCode.encodeText("Hello, world!", QrCode.Ecc.MEDIUM);
+		BufferedImage image = qr.toImage(4, 10);
+		Iterator<ImageWriter> writers = ImageIO.getImageWritersByMIMEType(contentType);
+		if (!writers.hasNext()) {
+			throw new IllegalArgumentException("No handler for the content type: " + contentType);
+		}
+		ImageWriter writer = writers.next();
+		ByteArrayOutputStream output = new ByteArrayOutputStream();
+		ImageOutputStream imageOutput = ImageIO.createImageOutputStream(output);
+		writer.setOutput(imageOutput);
+		writer.write(image);
+		imageOutput.flush();
+		return new ByteArrayInputStream(output.toByteArray());
+	}
+}
